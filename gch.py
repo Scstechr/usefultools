62171724d5016abddb59233522bcf01a4e806d65
Scstechr <teufelkonig@gmail.com>
2018-03-02 23:55:33 +0900

    class: issues

--- gch.py
+++ gch.py
@@ -6,7 +6,17 @@ import subprocess as sp
 import click
 import bcolors as bc
 
+class issues:
+    def BRANCH():
+        print(f'\n{bc.WARNING}>> BRANCH ISSUE!{bc.ENDC}')
+    def ABORT():
+        print(f'\n{bc.FAIL}>> ABORT!{bc.ENDC}')
+    def EXECUTE(command):
+        print(f'{bc.OKBLUE}>> EXECUTE: {command}{bc.ENDC}')
+        os.system(command)
+
 def cmd():
+    issues.BRANCH()
     print(dir(bc))
     pass
 
