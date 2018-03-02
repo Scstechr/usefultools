769d79700904b09828d85056d9c48e70073051c9
Scstechr <teufelkonig@gmail.com>
2018-03-03 00:56:11 +0900

    [gch][enhance]: def setBranch()

--- gch.py
+++ gch.py
@@ -18,21 +18,33 @@ class issues:
         if run:
             os.system(command)
 
-def getCurrentBranch():
+def getCurrentBranch(lst=False):
     ''' Returns current branch name '''
     l = sp.getoutput('git branch').split('\n')
     current_branch = ''.join(branch[2:] for branch in l if branch[0]=='*')
     branch_list = [branch[2:] for branch in l]
-    return current_branch, branch_list
+    if lst:
+        return current_branch, branch_list
+    else:
+        return current_branch
+
+def setBranch(branch):
+    current_branch, branch_list = getCurrentBranch(lst=True)
+    if branch not in branch_list:
+        print(f'Branch `{branch}` not found.')
+    else:
+        print(f'Currently on branch `{current_branch}`', end=' ')
+        print(f'but selected branch is `{branch}`.')
 
 @click.command()
 @click.option('--gitpath', default='.', type=click.Path(exists=True), help='Path of .git folder.    Default: .')
 @click.option('--filepath', default='.', type=click.Path(exists=True), help='Path of staing file(s). Default: .')
 @click.option('--branch', default='master', type=str, help='Commiting branch.       Default: master')
 def cmd(gitpath, filepath, branch):
-    current_branch, branch_list = getCurrentBranch()
+    current_branch = getCurrentBranch()
     if current_branch != branch:
         issues.BRANCH()
+        setBranch(branch)
     else:
         pass
 
