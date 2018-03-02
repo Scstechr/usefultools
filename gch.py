09a372993f1327b146ce2f1bdfb88e051f875ff7
Scstechr <teufelkonig@gmail.com>
2018-03-03 00:02:22 +0900

    def: getCurrentBranch

--- gch.py
+++ gch.py
@@ -13,12 +13,22 @@ class issues:
         print(f'\n{bc.WARNING}>> BRANCH ISSUE!{bc.ENDC}')
     def ABORT():
         print(f'\n{bc.FAIL}>> ABORT!{bc.ENDC}')
-    def EXECUTE(command):
+    def EXECUTE(command, run=False):
         print(f'{bc.OKBLUE}>> EXECUTE: {command}{bc.ENDC}')
-        os.system(command)
+        if run:
+            os.system(command)
+
+def getCurrentBranch():
+    ''' Returns current branch name '''
+    l = sp.getoutput('git branch').split('\n')
+    current_branch = ''.join(branch[2:] for branch in l if branch[0]=='*')
+    branch_list = [branch[2:] for branch in l]
+    return current_branch, branch_list
 
 def cmd():
     issues.BRANCH()
+    current_branch, branch_list = getCurrentBranch()
+    print(current_branch)
     pass
 
 def main():
