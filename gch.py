498f49dff6f12d93b5bb197facc11d61d54f81e2
Scstechr <teufelkonig@gmail.com>
2018-03-03 00:22:36 +0900

    [gch][enhance]: current_branch != branch statement

--- gch.py
+++ gch.py
@@ -30,9 +30,11 @@ def getCurrentBranch():
 @click.option('--filepath', default='.', type=click.Path(exists=True), help='Path of staing file(s). Default: "."')
 @click.option('--branch', default='master', type=str, help='Commiting branch. Default: "master"')
 def cmd(gitpath, filepath, branch):
-    issues.BRANCH()
     current_branch, branch_list = getCurrentBranch()
-    pass
+    if current_branch != branch:
+        issues.BRANCH()
+    else:
+        pass
 
 def main():
     cmd()
