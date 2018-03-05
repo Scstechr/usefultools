a015fe883a35b2b87d9f1b8bb8aa912e4a475221
Scstechr <teufelkonig@gmail.com>
2018-03-06 01:30:30 +0900

    myenv try

--- sptest.py
+++ sptest.py
@@ -1,4 +1,10 @@
 import subprocess as sp
 
-sp.call('git config --list', shell=True)
-sp.call('git push -u origin master', shell=True)
+import os, sys
+
+myenv = os.environ.copy()
+for key in myenv.keys():
+    print(key, '\t', myenv[key])
+    
+#sp.call('git config --list', shell=True)
+sp.call('git push -u origin master', shell=True, env=myenv)
