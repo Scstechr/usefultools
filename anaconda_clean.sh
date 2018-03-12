a7e2984fca102b2bbc5769313701a5daf1dfa46b
Scstechr <teufelkonig@gmail.com>
2018-03-13 06:21:36 +0900

    complete

---
+++ anaconda_clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "conda install anaconda-clean"
+conda install anaconda-clean;
+echo "anaconda-clean"
+anaconda-clean;
+echo "sudo rm -rf /usr/local/miniconda3"
+sudo rm -rf /usr/local/miniconda3;
+echo "sudo rm -rf /usr/local/Caskroom/miniconda"
+sudo rm -rf /usr/local/Caskroom/miniconda;
+echo "sudo rm -rf /Users/scstechr/.anaconda_backup/"
+sudo rm -rf /Users/scstechr/.anaconda_backup/
+echo "source ~/.bash_profile"
+source ~/.bash_profile
+
