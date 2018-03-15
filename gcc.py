0d7ba094a695bbdd8ccb5e5326ec992f36630a0b
Scstechr <teufelkonig@gmail.com>
2018-03-15 18:20:50 +0900

    gcc.py

---
+++ gcc.py
@@ -0,0 +1,9 @@
+#!/Users/moinaga/anaconda/bin/python
+import os, sys
+
+if len(sys.argv) == 2:
+    output = sys.argv[1][:sys.argv[1].find('.')] + '.out'
+    os.system(f'gcc -O3 -mtune=native -march=native {sys.argv[1]} -lm')
+    os.system(f'mv ./a.out ./{output}')
+    os.system(f'./{output}')
+    
