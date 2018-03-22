7ba47a786f12ceaf9c71ab594dea0e22d3617312
Scstechr <teufelkonig@gmail.com>
2018-03-23 02:33:01 +0900

    many changes

--- gch.py
+++ gch.py
@@ -45,7 +45,7 @@ def getAnswer(lst):
 
 def Commit():
     ''' Commit '''
-    commit_message = input('Commit Message: ')
+    commit_message = click.prompt("Commit Message", type=str)
     issues.EXECUTE([f'git commit -m "{commit_message}"'])
 
 def getCurrentBranch(lst=False):
@@ -61,18 +61,17 @@ def getCurrentBranch(lst=False):
 def setBranch(branch, filepath):
     current_branch, branch_list = getCurrentBranch(lst=True)
     if branch not in branch_list:
-        print(f'Branch `{b(branch)}` not found.')
+        issues.WARNING(f'Branch `{b(branch)}` not found.')
         qs =     [f'Make new branch `{b(branch)}`               ']
         qs.append(f'Stay on current branch `{b(current_branch)}`')
         answer = getAnswer(qs)
         if answer == 1:
             issues.EXECUTE([f'git checkout -b {branch}'])
         else:
-            print(f'Commiting branch set to {b(current_branch)}')
+            click.echo(f'Commiting branch set to {b(current_branch)}')
             branch = current_branch
     else:
-        text = f'Currently on branch `{b(current_branch)}` but tried to commit to branch `{b(branch)}`.'
-        print(text)
+        click.echo(f'Currently on branch `{b(current_branch)}` but tried to commit to branch `{b(branch)}`.')
         qs =     [f'Merge branch `{b(current_branch)}` => branch `{b(branch)}`']
         qs.append(f'Stay on branch `{b(current_branch)}`                   ')
         qs.append(f'Checkout to branch `{b(branch)}`                       ')
@@ -107,7 +106,20 @@ def isExist(command):
     flag = False if len(output) == 0 else True
     return flag
 
-def initialize():
+def initialize(flag=False):
+    # git confi
+    if not path.exists("~/.gitconfig") and flag:
+        click.echo("~/.gitconfig file does not exist. => Start Initialization!")
+        username = click.prompt("username", type=str)
+        email = click.prompt("email", type=str)
+        issues.EXECUTE([f'git config --global user.name "{username}"',\
+                        f'git config --global user.email {email}'])
+
+        if click.confirm('Do you want to use emacs instead of vim as an editor?'):
+            issues.EXECUTE([f'git config --global core.editor emacs'])
+        click.echo("# using vimdiff as a merge tool")
+        issues.EXECUTE([f'git config --global merge.tool vimdiff',\
+                         'git config --list'])
     title = input('Title of this repository(project): ').upper()
     issues.EXECUTE(['git init', 'touch .gitignore', 'touch README.md'])
     issues.EXECUTE(['echo ".*" >> .gitignore', f'echo "# {title}" >> README.md'])
@@ -142,9 +154,8 @@ def main(gitpath, filepath, branch, push, detail, log, commit, unstage):
     gitfolder = path.join(gitpath, '.git')
     if not path.exists(gitfolder):
         issues.WARNING(f'It seems path:`{gitpath}` does not have `.git` folder.')
-        answer = input(f'Initialize? [y/N]: ')
-        if answer == 'y':
-            initialize()
+        if click.confirm(f'Initialize?'):
+            initialize(True)
         else:
             issues.ABORT()
 
