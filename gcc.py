7fa4e3689d62bcf7d4818c1561b4f75ca0c30f59
Scstechr <teufelkonig@gmail.com>
2018-03-31 14:21:12 +0900

    [gcc] clang -> gcc

--- gcc.py
+++ gcc.py
@@ -3,8 +3,9 @@ import os, sys
 
 if len(sys.argv) == 2:
     output = sys.argv[1][:sys.argv[1].find('.')] + '.out'
-    #os.system(f'gcc -O3 -mtune=native -march=native {sys.argv[1]} -lm')
-    os.system(f'clang {sys.argv[1]}')
+    gcc = b'/usr/local/bin/gcc-7'
+    os.system(f'{gcc} -O3 -mtune=native -march=native {sys.argv[1]} -lm')
+    #os.system(f'clang {sys.argv[1]}')
     os.system(f'mv ./a.out ./{output}')
     os.system(f'./{output}')
     
