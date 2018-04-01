67e86d28cff42d08c6fad520b986d2b9f60357e9
Scstechr <teufelkonig@gmail.com>
2018-04-01 17:53:42 +0900

    gcc

--- gcc.py
+++ gcc.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, subprocess as sp
+from pysrc import issues
+import click
 
-if len(sys.argv) == 2:
-    output = sys.argv[1][:sys.argv[1].find('.')] + '.out'
-    gcc = '/usr/local/bin/gcc-7'
-    os.system(f'{gcc} -O3 -mtune=native -march=native {sys.argv[1]} -lm')
-    #os.system(f'clang {sys.argv[1]}')
-    os.system(f'mv ./a.out ./{output}')
-    os.system(f'./{output}')
+def f(string):
+    return f'\033[4m`{string}`\033[0m'
+
+@click.command()
+@click.argument('filename')
+@click.option('-d', '--debug', is_flag='False', help='LLDB MODE TRIGGER')
+def main(filename, debug):
+        output = filename[:filename.find('.')] + '.out'
+        print("filename:", filename);
+        gcc = '/usr/local/bin/gcc-7'
+        if debug:
+            issues.execute([f'{gcc} {filename} -g -O0 -m32'])
+            issues.execute([f'lldb ./a.out'])
+            if click.confirm(f"Remove {f('a.out')} & related directory?"):
+                issues.execute([f'rm ./a.out', f'rm -r ./a.out.dSYM'])
+        else:
+            issues.execute([f'{gcc} -o3 -mtune=native -march=native {filename} -lm'])
+            issues.execute([f'./{output}'])
+
+if __name__ == '__main__':
+    main()
     
