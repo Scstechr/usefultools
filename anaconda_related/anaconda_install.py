c52de6b6fe007fe86a01d401a5f2f936f662882b
Scstechr <teufelkonig@gmail.com>
2018-04-11 22:40:33 +0900

    updates

--- anaconda_related/anaconda_install.py
+++ anaconda_related/anaconda_install.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 import sys, subprocess as sp
 
+# installing brew
+if not os.path.exists('/usr/local/bin/brew'):
+    sp.call('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"', shell = True)
+
 if sys.version.find('Anaconda') < 0:
-    sp.call("echo 'Anaconda (or any other variation of it) is not installed'", shell=True)
-    answer = raw_input("Install Anaconda (Miniconda)?[y/N] ")
-    if answer == 'y':
-        sp.call('brew install Caskroom/cask/miniconda', shell=True)
-        sp.call('echo export PATH="/usr/local/miniconda3/bin:$PATH" >> ~/.bash_profile', shell=True)
-        sp.call('source ~/.bash_profile', shell=True)
-    else:
-        sp.call("echo 'Aborting Process'", shell=True)
+    sp.call('brew install Caskroom/cask/miniconda', shell=True)
+    sp.call('echo export PATH="/usr/local/miniconda3/bin:$PATH" >> ~/.bash_profile', shell=True)
+    sp.call('source ~/.bash_profile', shell=True)
 else:
     sp.call("echo 'Anaconda already installed'", shell=True)
